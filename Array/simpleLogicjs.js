0d6c9c0f66d80bc3ef13988b6ecc997b2c02ab52
Atul kumar singh <atulkumarsingh958@gmail.com>
2025-07-28 00:10:54 +0530

    logic

---
+++ Array/simpleLogicjs.js
@@ -0,0 +1,9 @@
+console.log(false == []);
+
+// false convert [] into empty string ""
+// false convert zero and empty string also convert zero then return true
+
+console.log(false == ![]);
+
+// in js [] is a truthi value but we negate then false == false get result is ture 
+

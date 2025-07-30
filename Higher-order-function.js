1a7d57a4e662a76a82ed427b706a04289720196d
Atul kumar singh <atulkumarsingh958@gmail.com>
2025-07-31 01:17:50 +0530

    change folder

--- Higher-order-function.js
+++ Higher-order-function.js
@@ -1,9 +1,6 @@
-
 let arr = [1, 2, 3, 4, 5];
 
-function filterarray(arr){
-    return arr.filterarray(num => num % 2===0);
+function filterarray(arr) {
+  return arr.filterarray((num) => num % 2 === 0);
 }
-console.log(filterarray
-    (arr)
-);
+console.log(filterarray(arr));
